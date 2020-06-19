29719254db6f854003bcc8d9dc28447911f268e4
Yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2020-06-19 07:39:07 -0500

    Added sanity_check() and various adjustments.
    Now I can check to see if we're regexing the result we already made from a previous run, meaning it's pointless and the result would be identical.

--- ATBS/phone_email_extractor.py
+++ ATBS/phone_email_extractor.py
@@ -59,9 +59,9 @@ def find_phone_matches(user_clipboard):
     We can change phone formatting here to whatever we want, regardless of
     how they were originally formatted.
     """
-
-    # For each phone number found with the regex scanning the clipboard content coming in:
-    for phone in phone_regex.findall(user_clipboard):
+    # For each phone number found with the regex scanning the clipboard content coming in,
+    # after first removing duplicates from that list:
+    for phone in remove_dupes(phone_regex.findall(user_clipboard)):
         # The way I format based on optional extensions and country codes seems really messy.
         # Feels too much like "choose your own adventure" here.
         # Not sure how to approach fixing that.
@@ -92,21 +92,26 @@ def find_phone_matches(user_clipboard):
 
 
 def find_email_matches(user_clipboard):
-    """
-    Find email regex matches, then append to list.
-    """
-
-    # For each email address found in clipboard:
-    for email in email_regex.findall(user_clipboard):
+    """Find email regex matches, then append to list."""
+    # For each email address found in clipboard list, while removing duplicates:
+    for email in remove_dupes(email_regex.findall(user_clipboard)):
         # Add group 0 from the regex, which is just the whole email address.
         email_matches.append(email[0])
 
 
-def copy_results():
+def remove_dupes(clean_me):
     """
-    Copy all results to the user's clipboard, if there are any.
+    This should remove any duplicate entries in the matches we pass in as the clean_me argument.
+    Since we can't have duplicate keys in a dictionary, we convert the list to dictionary keys,
+    then back to a list.
+    I end up pasting this function a lot below. I hope this is the best way to approach this.
     """
+    cleaned = list(dict.fromkeys(clean_me))
+    return cleaned
 
+
+def copy_results():
+    """Copy all results to the user's clipboard, if there are any."""
     # If either matches list is not empty:
     if len(email_matches) > 0 or len(phone_matches) > 0:
         # Join each result in the matches lists, to a string, with a new line \n.
@@ -114,14 +119,37 @@ def copy_results():
         # .join() actually iterates! I didn't know that.
         ph_results = '\n'.join(phone_matches)
         em_results = '\n'.join(email_matches)
-        # Combine and copy these results to the user's clipboard.
-        pyperclip.copy(f"{ph_results}\n{em_results}")
+        # Format for the clipboard result.
+        glorious_result = f"{ph_results}\n{em_results}"
 
-        # Since we've copied the results to the clipboard, we'll always then want to
+        # Since we'll copy the results to the clipboard, we'll always then want to
         #   - Show results for feedback so the user isn't confused about if anything even happened.
         #   - Clear the lists so we can run this as much as we want after.
-        show_results()
-        clear_matches()
+        #   - Make sure we haven't just copied/pasted the same results from a previous run.
+        check_me = str(pyperclip.paste())
+        if sanity_check(check_me) != "oh no":
+            # Combine and copy these results to the user's clipboard.
+            pyperclip.copy(glorious_result)
+            show_results()
+            clear_matches()
+
+        # If it is actually identical:
+        elif sanity_check(check_me) == "oh no":
+            sanity_confirm = input("\n\nWhoa there cowboy. You're checking the results of your last scan, "
+                                   "so it's going to be identical.\n"
+                                   "Are you sure you want to do this? y/n:\n")
+
+            if sanity_confirm != 'n':
+                # If yes, then go ahead and do it again for whatever reason.
+                pyperclip.copy(glorious_result)
+                show_results()
+                clear_matches()
+            else:
+                # Else reset.
+                print("---------------------------------------------------------------------------\n"
+                      "Canceling...\n"
+                      "---------------------------------------------------------------------------\n")
+                clear_matches()
 
     # If neither regex found any matches, then the lengths will be zero, so we come here.
     else:
@@ -131,9 +159,7 @@ def copy_results():
 
 
 def show_results():
-    """
-    Show us results were copied to the clipboard, for feedback.
-    """
+    """Show us results were copied to the clipboard, for feedback."""
     print(f"\nPhone Results:")
     for number in phone_matches:
         print(number)
@@ -143,7 +169,7 @@ def show_results():
         print(email)
 
     print(f"\nCopied {len(email_matches)+len(phone_matches)} results to your clipboard.\n"
-          f"Now you can paste it wherever you want.\n"
+          f"(Note, duplicates have been removed from results).\n"
           f"---------------------------------------------------------------------------\n")
 
 
@@ -155,11 +181,31 @@ def clear_matches():
     del phone_matches[:]
 
 
+def paste_clipboard():
+    """Paste out current clipboard and return it!"""
+    pasted_clipboard = str(pyperclip.paste())
+    return pasted_clipboard
+
+
+def sanity_check(string_to_check):
+    """
+    Check if what we're scanning is what we generated previously.
+    Returns "oh no" if it is identical.
+    """
+    ph_results = '\n'.join(phone_matches)
+    em_results = '\n'.join(email_matches)
+    glorious_result = f"{ph_results}\n{em_results}"
+
+    if string_to_check == glorious_result:
+        return "oh no"
+
+
 def press_play():
     """
     This is our looping function to allow repeated use of the program until we quit.
     """
-    print("This program will scan your clipboard content for any phone numbers and email addresses, "
+    print("----------------------------------------------------------------------------------------\n"
+          "This program will scan your clipboard content for any phone numbers and email addresses,\n"
           "then copy them to your clipboard as well as display the results.")
 
     while True:
@@ -168,10 +214,8 @@ def press_play():
                            "\nYou can also enter q to quit.\n")
 
         if user_input != 'q':
-            # Assign variable for our pasted clipboard that we'll pass into our functions.
-            pasted_clipboard = str(pyperclip.paste())
-            # Toss it into check_for_matches function.
-            check_for_matches(pasted_clipboard)
+            # Check for matches in our pyperclip pasted clipboard.
+            check_for_matches(paste_clipboard())
             copy_results()
         else:
             break
