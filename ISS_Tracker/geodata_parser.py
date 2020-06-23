202aea8c69114ae9809da0542bf062e982d76742
Yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2020-06-23 17:29:39 -0500

    Added more locations to scan, with bugs

--- ISS_Tracker/geodata_parser.py
+++ ISS_Tracker/geodata_parser.py
@@ -14,8 +14,11 @@ Either way, I also need to learn pandas..
     https://www.learndatasci.com/tutorials/python-pandas-tutorial-complete-introduction-for-beginners/
 """
 
+# Set acceptable degree difference here for returning coordinate matches.
+tolerance = 5
 
-def get_data_details(reader, iss_latitude, iss_longitude):
+
+def get_national_details(reader, iss_latitude, iss_longitude):
     # Checking to see what all the headers are, laid out neatly with their index visible.
     # header_row = next(reader)
     # for index, column_header in enumerate(header_row):
@@ -48,14 +51,84 @@ def get_data_details(reader, iss_latitude, iss_longitude):
     for index, row in enumerate(reader):
         # We need to make sure we aren't grabbing the header row, because then row values 9 and 10 aren't numbers.
         if index != 0:
-            # Grab each thing from each column. Convert degrees to floats so we can do math with them.
-            feature_name = row[1]
-            state_name = row[3]
-            location_latitude = float(row[9])
-            location_longitude = float(row[10])
+            try:
+                # Grab each thing from each column. Convert degrees to floats so we can do math with them.
+                feature_name = row[1]
+                state_name = row[3]
+                location_latitude = float(row[9])
+                location_longitude = float(row[10])
 
-            # See if we're close to this row's location, with an absolute value so it's never negative.
-            # One degree of latitude or longitude is about 111km. So maybe 0.2 degrees is around 20km.
-            if abs(iss_latitude - location_latitude) < 0.2 and abs(iss_longitude - location_longitude) < 0.2:
-                # Then return the name of this location the ISS is above!
-                return f"{feature_name} in {state_name}"
+                # See if we're close to this row's location, with an absolute value so it's never negative.
+                # One degree of latitude or longitude is about 111km. So maybe 0.2 degrees is around 20km.
+                # Five degrees would be ~555km.
+                if abs(iss_latitude - location_latitude) < tolerance and \
+                        abs(iss_longitude - location_longitude) < tolerance:
+                    # Then return the name of this location the ISS is above!
+                    return f"{feature_name} in {state_name}"
+
+            except ValueError:
+                # These data sets sometimes have a letter in the latitude/longitude cells.
+                # So if we run into that, just skip it.
+                continue
+
+
+def get_global_details(reader, iss_latitude, iss_longitude):
+    # Index: 0. Column Header: RC.
+    # Index: 1. Column Header: UFI.
+    # Index: 2. Column Header: UNI.
+    # Index: 3. Column Header: LAT.         <- Right here!
+    # Index: 4. Column Header: LONG.        <- And here!
+    # Index: 5. Column Header: DMS_LAT.
+    # Index: 6. Column Header: DMS_LONG.
+    # Index: 7. Column Header: MGRS.
+    # Index: 8. Column Header: JOG.
+    # Index: 9. Column Header: FC.
+    # Index: 10. Column Header: DSG.
+    # Index: 11. Column Header: PC.
+    # Index: 12. Column Header: CC1.
+    # Index: 13. Column Header: ADM1.
+    # Index: 14. Column Header: POP.
+    # Index: 15. Column Header: ELEV.
+    # Index: 16. Column Header: CC2.
+    # Index: 17. Column Header: NT.
+    # Index: 18. Column Header: LC.
+    # Index: 19. Column Header: SHORT_FORM.
+    # Index: 20. Column Header: GENERIC.            <- Clean name possibility.
+    # Index: 21. Column Header: SORT_NAME_RO.
+    # Index: 22. Column Header: FULL_NAME_RO.       <- Maybe this.
+    # Index: 23. Column Header: FULL_NAME_ND_RO.    <- Maybe this?
+    # Index: 24. Column Header: SORT_NAME_RG.
+    # Index: 25. Column Header: FULL_NAME_RG.
+    # Index: 26. Column Header: FULL_NAME_ND_RG.
+    # Index: 27. Column Header: NOTE.
+    # Index: 28. Column Header: MODIFY_DATE.
+    # Index: 29. Column Header: DISPLAY.
+    # Index: 30. Column Header: NAME_RANK.
+    # Index: 31. Column Header: NAME_LINK.
+    # Index: 32. Column Header: TRANSL_CD.
+    # Index: 33. Column Header: NM_MODIFY_DATE.
+    # Index: 34. Column Header: F_EFCTV_DT.
+    # Index: 35. Column Header: F_TERM_DT.
+
+    # Get Latitude[3], Longitude[4], and the landmark name [20, 22 or 23]
+    # For each row in the csv reader object that opened the file:
+    for index, row in enumerate(reader):
+        # We need to make sure we aren't grabbing the header row, because then coordinate row values aren't numbers.
+        if index != 0:
+            # Grab each thing from each column. Convert degrees to floats so we can do math with them.
+            try:
+                landmark_name = row[20]
+                location_latitude = float(row[3])
+                location_longitude = float(row[4])
+
+                # See if we're close to this row's location, with an absolute value so it's never negative.
+                # One degree of latitude or longitude is about 111km. So maybe 0.2 degrees is around 20km.
+                if abs(iss_latitude - location_latitude) < tolerance and \
+                        abs(iss_longitude - location_longitude) < tolerance:
+                    # Then return the name of this location the ISS is above!
+                    return f"{landmark_name}"
+
+            except ValueError:
+                # These data sets sometimes have a letter in the latitude/longitude cells.
+                # So if we run into that, just skip it.
+                continue
