25c532bf42e259bb4e68e046d22a32330698ee50
Yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2020-09-22 08:54:28 -0500

    various updates
    better logging
    better file naming
    swap from PyPDF2 to PyPDF4
    add timestamps

--- Python/VariousScripts/PDFExtract.py
+++ Python/VariousScripts/PDFExtract.py
@@ -1,5 +1,5 @@
-# This will extract text, images, and text inside those images
-# from a provided PDF file.
+# This will extract text, images, and text inside those images from a provided
+#   PDF file.
 #
 # Reference info:
 # AtBS Chapter 15.
@@ -14,29 +14,61 @@
 
 
 # Imports ----------------------------------------------------------------------
-import PyPDF2
+import PyPDF4
 import fitz
 import os
 import logging
-import datetime
+from datetime import datetime
+import pdfminer3
 # from PIL import Image
 # import pytesseract
 
 
+# Timestamp for the log files. -------------------------------------------------
+# Timestamp formatting is too messy to be used for file name:
+#   (2020-09-19 18:00:50.263646), so I'll make my own.
+# I'll also add zeros to ensure each slot is at least 2 digits long so that
+#   sorting by file name works.
+now = datetime.now()
+timestamp = datetime.timestamp(now)
+time_format = datetime.fromtimestamp(timestamp)
+
+
+def add_a_zero(number):
+    if len(str(number)) < 2:
+        number = f"0{number}"
+        return number
+    else:
+        return number
+
+
+year   = time_format.year
+month  = add_a_zero(time_format.month)
+day    = add_a_zero(time_format.day)
+hour   = add_a_zero(time_format.hour)
+minute = add_a_zero(time_format.minute)
+second = add_a_zero(time_format.second)
+
+time = f"{year}-{month}-{day} {hour}.{minute}.{second}"
+
+
 # Logging ----------------------------------------------------------------------
-# The console spam is way too fast and inconvenient for me to review. ----------
+# The console spam is way too fast for me to review!
 # https://stackoverflow.com/a/17682520/13627106
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger()
 # Shadowing print here is intentional so all prints are replaced with logging.
 print = logger.info
-log_folder = f'{os.getcwd()}/output_logs'
-logger.addHandler(logging.FileHandler(f'{log_folder}/PDFExtract_log.txt', 'w'))
+log_folder = f'{os.getcwd()}/PDFExtract Logs'
 
 try:
     os.makedirs(log_folder)
 except FileExistsError:
     pass
+
+logger.addHandler(logging.FileHandler(
+    f'{log_folder}/PDFExtract log {time}.txt',
+    'w'))
 
 
 # Classes ----------------------------------------------------------------------
@@ -62,7 +94,7 @@ class PDFAnalyzer:
         """
         pdf_doc = fitz.open(self.pdf_full_path)
         # New folder for extracted images.
-        img_folder_path = f"{self.directory}/extracted_images"
+        img_folder_path = f"{self.directory}/extracted images"
         try:
             os.makedirs(img_folder_path)
         except FileExistsError:
@@ -76,7 +108,7 @@ class PDFAnalyzer:
                     print(f"Found {img[-1]} image on page {page}.")
                     xref = img[0]
                     pix = fitz.Pixmap(pdf_doc, xref)
-                    image_name = f"{img_folder_path}/p{page}-{xref}.png"
+                    image_name = f"{img_folder_path}/p{page} x{xref}.png"
 
                     # Save images depending on color map type. -----------------
                     if pix.n - pix.alpha < 4:
@@ -91,7 +123,8 @@ class PDFAnalyzer:
                         print(f"Saved image: {image_name}")
 
             except RuntimeError:
-                print(f"Cannot retrieve page info for page {page} in {self.file_name}.")
+                print(f"\n/////////// Image Extract: Cannot retrieve page info "
+                      f"for page {page} in {self.file_name}.\n")
                 continue
 
     def text_extract_separated(self):
@@ -101,7 +134,7 @@ class PDFAnalyzer:
         """
 
         # New folder for extracted pages.
-        text_folder_path = f"{self.directory}/extracted_text"
+        text_folder_path = f"{self.directory}/extracted text"
         try:
             os.makedirs(text_folder_path)
         except FileExistsError:
@@ -109,22 +142,34 @@ class PDFAnalyzer:
 
         # Open PDF as object to start. -----------------------------------------
         with open(self.pdf_full_path, 'rb') as pdf_file:
-            reader = PyPDF2.PdfFileReader(pdf_file)
+            reader = PyPDF4.PdfFileReader(pdf_file)
             self.page_amt = reader.numPages
-            print(f"Found {self.page_amt} pages in {self.file_name}.")
+            print(f"\nFound {self.page_amt} pages in {self.file_name}.\n")
 
             # Extract text on each page.
             for p in range(1, reader.numPages):
                 page = reader.getPage(p)
-                extracted_text = page.extractText()
+
+                try:
+                    extracted_text = page.extractText()
+                except KeyError:
+                    print(f"\n/////////// Text Extract: "
+                          f"'extractText' resulted in a Key Error on page "
+                          f"{p} of {pdf_file}.\n")
+                    continue
 
                 # Save text extracts to txt files.
-                with open(f"{text_folder_path}/{self.file_name[:-4]}_page{p}.txt",
+                with open(f"{text_folder_path}/{self.file_name[:-4]} page {p}.txt",
                           "w", encoding="utf-8") as text_page:
 
                     print(f"Creating text file of page {p} of {reader.numPages} "
-                          f"to {self.file_name[:-4]}_page{p}.txt.")
-                    text_page.write(extracted_text)
+                          f"to {self.file_name[:-4]} page {p}.txt.")
+                    text_page.write(f"\n\n\n\n"
+                                    f"---------------------------- "
+                                    f"Page {p} of {reader.numPages} "
+                                    f"----------------------------"
+                                    f"\n\n\n\n"
+                                    f"{extracted_text}")
 
     def text_extract_combined(self):
         """
@@ -133,7 +178,7 @@ class PDFAnalyzer:
         """
 
         # New folder for extracted pages.
-        text_folder_path = f"{self.directory}/extracted_text"
+        text_folder_path = f"{self.directory}/extracted text"
         try:
             os.makedirs(text_folder_path)
         except FileExistsError:
@@ -141,22 +186,42 @@ class PDFAnalyzer:
 
         # Open PDF as object to start. -----------------------------------------
         with open(self.pdf_full_path, 'rb') as pdf_file:
-            reader = PyPDF2.PdfFileReader(pdf_file)
+            reader = PyPDF4.PdfFileReader(pdf_file)
             self.page_amt = reader.numPages
-            print(f"Found {self.page_amt} pages in {self.file_name}.")
+            print(f"\nFound {self.page_amt} pages in {self.file_name}.\n")
 
             # Extract text on each page.
             for p in range(1, reader.numPages):
                 page = reader.getPage(p)
-                extracted_text = page.extractText()
+
+                try:
+                    extracted_text = page.extractText()
+                except KeyError:
+                    print(f"\n/////////// Extracting text failed on page "
+                          f"{p} of {pdf_file}.\n")
+                    continue
 
                 # Save text extracts to text file, append.
-                with open(f"{text_folder_path}/{self.file_name[:-4]}_full.txt",
+                with open(f"{text_folder_path}/{self.file_name[:-4]} full.txt",
                           "a", encoding="utf-8") as text_page:
 
-                    print(f"Adding text from page {p} of {reader.numPages} "
-                          f"to {self.file_name[:-4]}_full.txt.")
-                    text_page.write(extracted_text)
+                    if len(extracted_text) > 0:
+                        text_page.write(f"\n\n\n\n"
+                                        f"---------------------------- "
+                                        f"Page {p} of {reader.numPages} "
+                                        f"----------------------------"
+                                        f"\n\n\n\n"
+                                        f"{extracted_text}")
+
+                        print(f"Adding text from page {p} of {reader.numPages} "
+                              f"to {self.file_name[:-4]} full.txt.")
+
+                    elif len(extracted_text) == 0:
+                        text_page.write(f"\nMISSING OR NO TEXT ON PAGE "
+                                        f"{p} of {reader.numPages}.\n")
+
+                        print(f"/////// Page {p} of {reader.numPages} "
+                              f"in {self.file_name} did not generate any text.")
 
     def tesseract_extract(self):
         """
@@ -175,7 +240,7 @@ def run_extracts(*pdfa_instances):
     for every instance I make for each PDF I wanna analyze.
     """
     for instance in pdfa_instances:
-        instance.text_extract_separated()
+        # instance.text_extract_separated()
         instance.text_extract_combined()
         instance.image_extract()
         instance.tesseract_extract()
@@ -204,5 +269,5 @@ crash_course = PDFAnalyzer(
 # Start ------------------------------------------------------------------------
 # Pass in instances to function that runs all the methods I wanna run.
 # TODO: Automatically create list/dict of instances that passes into this.
-run_extracts(crash_course, meeting_minutes, automate_boring)
-print(f"Complete! Check log files at {log_folder}.")
+run_extracts(meeting_minutes, automate_boring, crash_course)
+print(f"\nComplete! Check log files at {log_folder}.\n")
