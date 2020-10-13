3afc33625d8acb3fabf64407768453fc72f9e3d9
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2020-10-13 01:42:01 -0500

    - Catch KeyError. (cont.) - Adjust predicted file size. - Simplified some code.

--- Python/Scripts/ForAutomation/ArjunaCollabs/shingekiscraper.py
+++ Python/Scripts/ForAutomation/ArjunaCollabs/shingekiscraper.py
@@ -79,7 +79,7 @@ while True:
         # Check to see if we've already asked this or not.
         if not confirmation:
             # During tests, most chapters average ~25-30mb in folder size.
-            input(f"There are ~{length} chapters ready to download. \n"
+            input(f"There are ~{length} chapters available. \n"
                   f"Total download size may be around {(30 * length) / 1000}GB"
                   f" or larger."
                   f"\nHit enter to proceed.")
