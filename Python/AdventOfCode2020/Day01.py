3f714eadb4309c134356661134478cb2e146190f
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2020-12-02 16:42:30 -0600

    Removing magic numbers.

--- Python/AdventOfCode2020/Day01.py
+++ Python/AdventOfCode2020/Day01.py
@@ -1,6 +1,7 @@
 # Advent of Code Day 1.
 
 def find_twenty_twenty():
+    goal = 2020
     with open('M:\Coding Content\input.txt', 'r') as file:
         check_me = file.readlines()
 
@@ -8,16 +9,16 @@ def find_twenty_twenty():
         # but it works so I'll just go with it lol.
         for num_a in check_me:
             # Trying to cut down on *some* unnecessary searching here.
-            if int(num_a) < 2020:
+            if int(num_a) < goal:
                 for num_b in check_me:
                     # Further limiting search.
-                    if int(num_a) + int(num_b) < 2020:
+                    if int(num_a) + int(num_b) < goal:
                         for num_c in check_me:
                             a = int(num_a.strip())
                             b = int(num_b.strip())
                             c = int(num_c.strip())
 
-                            if a + b + c == 2020:
+                            if a + b + c == goal:
                                 return a * b * c
 
 
