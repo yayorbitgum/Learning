df6a2edfc567161cb52f8b9eb5cb9a4892885d02
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2020-12-03 17:12:12 -0600

    Advent of Code Day 3!

--- Python/AdventOfCode2020/Day03.py
+++ Python/AdventOfCode2020/Day03.py
@@ -1,6 +1,11 @@
 # https://adventofcode.com/2020/day/3#part1
+# https://adventofcode.com/2020/day/3#part2
+import math
 
-step = 3
+# ------------------------------------------------------------------------------
+step = 3    # This only works for part one. For part 2 it's not long enough.
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+results = []
 
 # ------------------------------------------------------------------------------
 with open('M:\Coding Content\day03_input.txt', 'r') as file:
@@ -15,13 +20,13 @@ with open('M:\Coding Content\day03_input.txt', 'r') as file:
 
     for line in file_lines:
         line = line.strip()
-        line *= biome_expansion
+        line *= biome_expansion * 10    # "10" is a hamfisted fix for part 2, for now lol.
         full_terrain.append(line)
 
 
 # ------------------------------------------------------------------------------
-def toboggan_trees(terrain, start=0, tree_count=0, segment=0):
-    end = start + 3
+def toboggan_trees(terrain, slope: tuple, start=0, tree_count=0, segment=0):
+    end = start + slope[0]
 
     if segment > len(terrain) - 1:
         return tree_count
@@ -29,10 +34,13 @@ def toboggan_trees(terrain, start=0, tree_count=0, segment=0):
     for obstacle in terrain[segment][start:end]:
         if obstacle == '#':
             tree_count += 1
-        segment += 1
+        segment += slope[1]
         start = end
-        return toboggan_trees(terrain, start, tree_count, segment)
+        return toboggan_trees(terrain, slope, start, tree_count, segment)
 
 
 # ------------------------------------------------------------------------------
-print(toboggan_trees(full_terrain))
+for slope in slopes:
+    results.append(toboggan_trees(full_terrain, slope))
+
+print(math.prod(results))
