81d2feb2d96fe0757fae13a2b339d7fd11382c1b
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-02-02 04:15:22 -0600

    Added Notes:  - Solution for finding longest common prefix

--- Python/Scripts/Notes/problems/LC_LongestCommonPrefix.py
+++ Python/Scripts/Notes/problems/LC_LongestCommonPrefix.py
@@ -5,6 +5,8 @@ from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
+        # I want to sort by shortest string first so we can break the loop sooner,
+        # in case a whole word is the whole prefix.
         strs.sort(key=len)
         prefix = ''
         if len(strs) == 0:
@@ -13,28 +15,30 @@ class Solution:
             return strs[0]
         # We're going to check each letter of each word, in order (via indexes).
         longest = len(max(strs))
+
         if longest == 0:
             # This will catch any lists of empty strings.
             return ''
+
         for index in range(longest):
             # Reset these values for each loop.
             letter = ''
-            count = 0
+            words_checked = 0
             letters = []
             # Check each word.
             for word in strs:
-                count += 1
+                words_checked += 1
                 try:
                     # Grab current letter.
                     letter = word[index]
                 except IndexError:
                     # Reaching here means a whole word is the prefix,
-                    # and we've already found it.
+                    # and we've already found it. This is why we sorted by length earlier.
                     return prefix
                 # Add the current letter to the list of letters. We want them all
                 # to match if it is a common prefix.
                 letters.append(word[index])
-                if letters.count(letter) != count:
+                if letters.count(letter) != words_checked:
                     # We'll reach here if there's a mismatch of a letter at any point,
                     # So we can stop the loop from running more than it needs to.
                     break
