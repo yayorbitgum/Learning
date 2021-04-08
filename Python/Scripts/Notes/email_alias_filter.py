975ba438173175753ddd9a7831a19dd4d2a3ef24
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-04-07 21:16:12 -0500

    Discussion, solution and notes

--- Python/Scripts/Notes/email_alias_filter.py
+++ Python/Scripts/Notes/email_alias_filter.py
@@ -5,7 +5,7 @@
 
 
 email_1 = 'leak+paypal@gmail.com'
-email_2 = 'leak+bank3e+w+fd+k@gmail.com'
+email_2 = 'leak+bank3e+w+fd$%*#()*%+k@gmail.com'
 email_3 = 'leak+my+favorite+egirl@gmail.com'
 
 emails = [email_1, email_2, email_3]
@@ -16,7 +16,10 @@ cleaned = []
 for email in emails:
     if '+' in email:
         blocks = email.split('+')
+        # The very first block before the first split is always gonna be the name.
         name = blocks[0]
+        # The very last block is always going to be the domain, but may contain
+        # extra characters after the last '+' split.
         domain_split = blocks[-1].split('@')
         domain = domain_split[-1]
 
