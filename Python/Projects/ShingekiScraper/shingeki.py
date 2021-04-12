a27fe7f1913f54fa3363423efc0ea6313f1cb506
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-04-12 16:08:18 -0500

    Starting code cleanup / refactoring.

--- Python/Projects/ShingekiScraper/shingeki.py
+++ Python/Projects/ShingekiScraper/shingeki.py
@@ -1,5 +1,5 @@
-#! python3
-# shingeki.py - Download every single shingeki no kyojin comic.
+# Scrapes and downloads all images from readnsk.com for Attack on Titan.
+# Organizes saved images by chapter and page in folders.
 #
 # Personal notes/references. ---------------------------------------------------
 # Dissecting HTML pages with BeautifulSoup:
@@ -9,48 +9,63 @@
 # Saving image data to a file:
 # https://stackoverflow.com/questions/54338681/how-to-download-images-from-websites-using-beautiful-soup
 
-# Imports/using. ---------------------------------------------------------------
+# Imports. ---------------------------------------------------------------------
 import os
 import requests
 from bs4 import BeautifulSoup
 from requests.exceptions import MissingSchema
 
 # Variables. -------------------------------------------------------------------
-ch_num = 0
+ch_number = 0
 ch_count = 0
 confirmation = False
 root = os.path.abspath(os.curdir)
 
 
 # Functions. -------------------------------------------------------------------
-def format_check(chapter_or_page):
-    """Formats number to three digits."""
+def format_check(ch_num: int) -> str:
+    """
+    Formats page number to three digits. Returns new string.
+    Used for generating URLs for scraping, and for formatting for saving files.
+    """
+    ch_num = f"{ch_num}"
+    if ch_num == "0":
+        # https://ww7.readsnk.com/chapter/shingeki-no-kyojin-chapter-0/
+        # compared to
+        # https://ww7.readsnk.com/chapter/shingeki-no-kyojin-chapter-001/
+        # So if it's chapter zero, don't format.
+        return ch_num
 
-    # https://ww7.readsnk.com/chapter/shingeki-no-kyojin-chapter-0/
-    # compared to...
-    # https://ww7.readsnk.com/chapter/shingeki-no-kyojin-chapter-001/
-    # So if it's chapter zero, don't format.
-    if chapter_or_page == 0:
-        return chapter_or_page
+    while len(ch_num) < 3:
+        ch_num = "0" + ch_num
 
-    # Format all the others.
-    if len(str(chapter_or_page)) == 1:
-        chapter_or_page = f"00{chapter_or_page}"
+    return ch_num
 
-    elif len(str(chapter_or_page)) == 2:
-        chapter_or_page = f"0{chapter_or_page}"
 
-    return chapter_or_page
+def get_chapter_count(soup_obj: BeautifulSoup) -> int:
+    """
+    Get a chapter count indirectly from the dropdown menu on the first page.
+    Return chapter count.
+    """
+    ch = []
+    for o in soup_obj.find_all('option'):
+        if "chapter" in o['value']:
+            ch.append(o)
+
+    # There are multiple dropdown menus (top and bottom) so let's remove duplicates.
+    length = len((dict.fromkeys(ch)))
+
+    return length
 
 
 # Chapter loop. ----------------------------------------------------------------
+# TODO: Break all of this apart into more modular components/functions.
 while True:
     # Reset page number and count for each chapter.
     page_number = 1
     count = 0
 
-    # Get chapter URL.
-    ch_format = format_check(ch_num)
+    ch_format = format_check(ch_number)
     ch_url = f'https://ww7.readsnk.com/chapter/shingeki-no-kyojin-chapter-{ch_format}/'
     req = requests.get(ch_url)
 
@@ -65,23 +80,11 @@ while True:
         except FileExistsError:
             print(f"Directory exists for {ch_path}.")
 
-        # Total chapter count confirmation. ------------------------------------
-        # Getting a rough chapter count from the dropdown menu on the first page.
-        list_of_chapters = []
-        for o in soup.find_all('option'):
-            if "chapter" in o['value']:
-                list_of_chapters.append(o)
-
-        # Remove duplicates (multiple identical dropdown menus on the website).
-        list_of_chapters = (list(dict.fromkeys(list_of_chapters)))
-
-        # Check to see if we've already asked this or not.
         if not confirmation:
-            length = len(list_of_chapters)
-            # During tests, most chapters average ~25-30mb in folder size.
-            input(f"There are ~{length} chapters available. \n"
-                  f"Total download size may be around {(30 * length) / 1000}GB"
-                  f" or larger."
+            chapters = get_chapter_count(soup)
+            # Most chapters average 25-30mb in folder size.
+            input(f"There are ~{chapters} chapters available. \n"
+                  f"Total download size may be around {(30 * chapters) / 1000}GB or larger."
                   f"\nHit enter to proceed.")
             confirmation = True
 
@@ -89,14 +92,14 @@ while True:
         # All manga pages are images.
         img_tags = soup.find_all('img')
         for img in img_tags:
-            page_number += 1
             try:
                 # The img class for the manga pages seems to always be this type.
                 if img['class'] == ['my-3', 'mx-auto', 'js-page']:
-                    # Grab the image url from "src" tag.
+                    # Grab image url from src tag.
                     url = img['src'].rstrip('\r')
 
                     # Save manga page. -----------------------------------------
+                    page_number += 1
                     pg_format = format_check(page_number)
                     with open(f"{ch_path}/Shingeki_{ch_format}_{pg_format}.png", 'wb') as file:
                         print(f"Downloading {url}...")
@@ -108,23 +111,19 @@ while True:
                             print(f"(404) Unable to find that image.\nSkipping!\n")
 
             except KeyError:
-                # Very rarely, a chapter may contain an image that has no key 'class'.
-                # These are manually placed ads what from I can tell.
-                print(f"Skipping: {img['src']}.")
+                # Very rarely, a chapter may contain an image (ad) that has no key 'class'.
+                print(f"Skipping: {img['src']} (ad).")
 
-        print(f"Finished chapter {ch_num}. Saved {count} pages.")
-        ch_num += 1
+        print(f"Finished chapter {ch_number}. Saved {count} pages.")
+        ch_number += 1
         ch_count += 1
 
     # Else "Not found" (404) status. -------------------------------------------
     elif req.status_code == 404:
-        print(f"Results: ------------------------------------------------------\n"
-              f"You downloaded a total of {ch_count} chapters.\n\n")
-        print(f"Unable to locate chapter {ch_num} of Shingeki at:")
-        print(ch_url)
+        print(f"You downloaded a total of {ch_count} chapters.\n\n")
+        print(f"Unable to locate chapter {ch_number} of Shingeki at\n {ch_url}")
         print(f"Does that chapter exist yet?\n"
               f"If it does exist, check for differences in the URLs for this chapter.")
-        print(f"Shutting down program.")
 
         # Open folder in explorer to see results (only works in Windows).
         os.startfile(root)
