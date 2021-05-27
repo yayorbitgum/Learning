8a63bb37ec3e856ed52db44f936437906b68c4bb
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-05-27 17:11:23 -0500

    Add C++ pointer notes.

--- CPP/Notes/CPPNotes/Pointers.cpp
+++ CPP/Notes/CPPNotes/Pointers.cpp
@@ -3,7 +3,6 @@
 int main()
 {
     int number = 25;
-
     // A pointer to an int, a reference. Pointers are made with "*".
     int* pointer;
     // This stores the address of "number". Get the address with "&".
