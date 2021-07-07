91f7fe20c401ee042732f3773563a92a4a09db99
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-07-06 23:39:27 -0500

    Add notes on small script for refererence.

--- Python/Scripts/Automation/MilliaModReorganizing.py
+++ Python/Scripts/Automation/MilliaModReorganizing.py
@@ -1,20 +1,21 @@
-# Wrote a quick script to rename and reorganize a torrent of DBZ episodes.
-
 import os
-import shutil
 
-GGStrive_folder = 'S:\Games\Steam\steamapps\common\GUILTY GEAR STRIVE\RED\Content\Paks\~mods'
+ggs_folder = 'S:\Games\Steam\steamapps\common\GUILTY GEAR STRIVE\RED\Content\Paks\~mods'
 
-for root, dirs, files in os.walk(GGStrive_folder):
-    print(root)
+count = 0
+
+for root, dirs, files in os.walk(ggs_folder):
 
     for file in files:
         if file.lower().__contains__("millia"):
+            count += 1
             replaced = file.lower().replace("millia", "")
             fixed = "Millia - " + replaced
             path_old = os.path.join(root, file)
             path_new = os.path.join(root, fixed)
-            print(path_old)
-            print(path_new)
-            print()
             os.rename(path_old, path_new)
+            print(f"From -> {path_old}")
+            print(f"To -> {path_new}\n")
+
+if count == 0:
+    print("No files found related to Millia.")
